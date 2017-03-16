1523c109457d06afa98847864165c032444aa487
Tyler Field <tfield@pointblue.org>
2017-03-16 13:43:00 -0700

    enhance footer links

--- src/Views/blades/pb-footer.blade.php
+++ src/Views/blades/pb-footer.blade.php
@@ -1,7 +1,23 @@
 <footer class="section section-primary" >
 	<div class="container container-fluid">
 		<div class="pull-left">
-			<a href="/apps/authentication/pb_logout.php?app={{urlencode(env('PB_APP_NAME'))}}&returnURL={{urlencode(env('APP_URL'))}}">Logout</a>
+			<ul class="list-inline" style="padding-top:1em;">
+				<li>
+					<a href="{{env('APP_URL')}}/feedback">Feedback</a>
+				</li>
+				<li>
+					<a href="{{env('APP_URL')}}/release">Release Information</a>
+				</li>
+				<li>
+					<a href="{{env('APP_URL')}}/help">Help</a>
+				</li>
+				<li>
+					Logged in as @{{ currentUserId }}
+				</li>
+				<li>
+					<a href="/apps/authentication/pb_logout.php?app={{urlencode(env('PB_APP_NAME'))}}&returnURL={{urlencode(env('APP_URL'))}}">Logout</a>
+				</li>
+			</ul>
 		</div>
 		<div class="pull-right">
 			<a href="http://www.pointblue.org/our-science-and-services/conservation-tools/data-solutions/">Powered by Point Blue Data Solutions</a>
