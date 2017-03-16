f5e66be7b5674a179627fec7847579213bfe30ae
Tyler Field <tfield@pointblue.org>
2017-03-16 12:11:58 -0700

    makes footer full width

--- src/Views/blades/pb-footer.blade.php
+++ src/Views/blades/pb-footer.blade.php
@@ -1,9 +1,11 @@
-<footer class="container" style="margin-top: 15em; margin-bottom: 2em;">
-	<div class="pull-left">
-		<a class="text-right" href="/apps/authentication/pb_logout.php?app={{urlencode(env('PB_APP_NAME'))}}&returnURL={{urlencode(env('APP_URL'))}}">Logout</a>
-	</div>
-	<div class="pull-right">
-		<a href="http://www.pointblue.org/our-science-and-services/conservation-tools/data-solutions/">Powered by Point Blue Data Solutions</a>
-		<a href="http://www.pointblue.org"><img src="images/pb-logo-full.png" height="60px"></a>
+<footer class="section section-primary" >
+	<div class="container container-fluid">
+		<div class="pull-left">
+			<a href="/apps/authentication/pb_logout.php?app={{urlencode(env('PB_APP_NAME'))}}&returnURL={{urlencode(env('APP_URL'))}}">Logout</a>
+		</div>
+		<div class="pull-right">
+			<a href="http://www.pointblue.org/our-science-and-services/conservation-tools/data-solutions/">Powered by Point Blue Data Solutions</a>
+			<a href="http://www.pointblue.org"><img src="images/pb-logo-full.png" height="60px"></a>
+		</div>
 	</div>
 </footer>
